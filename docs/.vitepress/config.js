2335c8bc07ed9f8387a5b20eb929c54a350054ad
CKY <awa@outlook.ie>
2022-11-06 11:35:07 +0800

    Docs

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,22 @@
+export default {
+    title: 'WeatherAllInOne',
+    description: 'WeatherAllInOne 是一款可以方便汇聚天气 API 为统一格式的工具。',
+    themeConfig: {
+        sidebar: [
+            {
+            text: '开始',
+            items: [
+            { text: '开始使用', link: '/start.html' },
+            ]},
+            {
+                text: 'API',
+                items: [
+                { text: '接口示例', link: '/api/' },
+                ]},
+        ],
+        footer: {
+            message: 'Released under the MIT License.',
+            copyright: 'Copyright © 2022 CKY'
+        }
+    }
+}
---
+++ docs/index.md
@@ -0,0 +1,14 @@
+---
+layout: home
+hero:
+  name: WAIO.js
+  text: Weather-All-In-One
+  tagline: WeatherAllInOne 是一款可以方便汇聚天气 API 为统一格式的工具。
+  actions:
+    - theme: brand
+      text: 开始
+      link: /start.html
+    - theme: alt
+      text: 在 GitHub 上查看
+      link: https://github.com/oCoke/WeatherAllInOne
+---
